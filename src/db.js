ffa286c98103e55ef62636e9edf2432c69cef9f1
Ajay KC <ajaykc90@gmail.com>
2021-08-15 18:15:28 +1000

    First-upload

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const github = {
+    baseUrl: "",
+    username: "",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: "bearer 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",
+    }
+}
+
+export default github;
